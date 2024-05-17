30b3d02f46c13d8d1aa72e7149169608d657a4ac
moradpablo <moradpabloivan@gmail.com>
2024-05-17 16:32:11 -0300

    CAC: modals for signup and login added

---
+++ src/script.js
@@ -0,0 +1,33 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const modal = document.getElementById('modal');
+  const loginBtn = document.getElementById('login-btn');
+  const signupBtn = document.getElementById('signup-btn');
+  const titleLogin = document.querySelector('.title-login');
+  const titleSignUp = document.querySelector('.title-signup');
+  const formLogin = document.querySelector('.form-login');
+  const formSignUp = document.querySelector('.form-signup');
+
+  loginBtn.addEventListener('click', function (e) {
+    e.preventDefault();
+    modal.style.display = 'flex';
+    titleLogin.style.display = 'block';
+    titleSignUp.style.display = 'none';
+    formSignUp.style.display = 'none';
+    formLogin.style.display = 'block';
+  });
+
+  signupBtn.addEventListener('click', function (e) {
+    e.preventDefault();
+    modal.style.display = 'flex';
+    titleSignUp.style.display = 'block';
+    titleLogin.style.display = 'none';
+    formSignUp.style.display = 'block';
+    formLogin.style.display = 'none';
+  });
+
+  window.onclick = function (event) {
+    if (event.target == modal) {
+      modal.style.display = 'none';
+    }
+  };
+});
